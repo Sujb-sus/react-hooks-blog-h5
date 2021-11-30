e4487b9c65d09897fb9c77448cae47383540fbf3
wall <854067626@qq.com>
2021-11-30 22:55:51 +0800

    article detail

--- src/pages/article/index.jsx
+++ src/pages/article/index.jsx
@@ -1,8 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { apiGetBlogDetail, apiUpdatePV } from '@/api/blog';
+import SvgIcon from '@/components/svgIcon';
+import { formatTime, formatNumber } from '@/utils/filter';
+import base from '@/utils/base';
+import './article.scss';
+import '@/styles/markdown/index.scss';
 import { useParams } from 'react-router-dom';
 
 export default function Article() {
   let params = useParams();
-  console.log('params', params);
-  return <div className="app-container">detail</div>;
+  let id = params.id;
+  let [detail, setDetail] = useState(null);
+
+  useEffect(async () => {
+    base.showLoading();
+    await updatePV();
+    await getBlogDetail();
+    base.hideLoading();
+  }, []);
+
+  // 获取文章详情
+  const getBlogDetail = () => {
+    return apiGetBlogDetail({ _id: id })
+      .then((res) => {
+        res?.data && setDetail(res.data);
+      })
+      .catch((err) => console.log('err', err))
+      .finally(() => {});
+  };
+  // 更新浏览量
+  const updatePV = () => {
+    return apiUpdatePV({ _id: id })
+      .then(() => {})
+      .catch((err) => console.log(err));
+  };
+  return (
+    detail && (
+      <div className="detail-content">
+        <div className="detail-body">
+          <div className="detail-title">{detail.title}</div>
+          <div className="detail-func">
+            <div className="func-icon">
+              <SvgIcon name="icon-date02"></SvgIcon>
+              <div className="box-text">
+                {formatTime(detail.releaseTime, 'yyyy-MM-dd')}
+              </div>
+            </div>
+            <div className="func-icon">
+              <SvgIcon name="icon-browse02"></SvgIcon>
+              <div className="box-text">{formatNumber(detail.pv)}</div>
+            </div>
+            {detail.auth && (
+              <div className="func-icon">
+                <SvgIcon name="icon-laborer"></SvgIcon>
+                <div className="box-text">{detail.auth}</div>
+              </div>
+            )}
+          </div>
+          <div
+            className="detail-main fmt"
+            dangerouslySetInnerHTML={{
+              __html: detail.html.replace(/<a /gi, `<a target='_blank'`),
+            }}></div>
+          {/* <div className="detail-label">
+        标签：
+        <div
+          className="box-text label-text"
+          :style="{ backgroundColor: getLabelColor(label) }"
+          v-for="label in detail.type"
+          :key="label"
+        >
+          { label }
+        </div>
+      </div> */}
+        </div>
+      </div>
+    )
+  );
 }
